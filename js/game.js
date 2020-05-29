f9ee13ce9adc6d91d585824338fca29ca277d366
NKRUS <nik1rus2moscow3@gmail.com>
2020-05-29 20:15:09 +0300

    Выбор рандомного слова, деление на буквы, сокрытие неугаданных букв *

--- js/game.js
+++ js/game.js
@@ -1,3 +1,15 @@
+const words = ["корова", "молоко", "динозавр"]
+var wordInfo = {
+    "word": [],
+    "guessIndexes": []
+}
+
+// *о*о*а
+// {   var userWord = "*о"
+//     "word": ["к", "о", "р", "о", "в", "а"],
+//     "guessIndexes": [1, 3, 5]
+// }
+
 // Считывает имя пользователя и начинает игру
 function readAndStart() {
     //Считать имя
@@ -8,6 +20,7 @@ function readAndStart() {
     startGame()
 }
 
+// Возврат имени пользователя из поля
 function readName() {
     let userName = $("#name-input").val()
     if (userName.length == 0){
@@ -18,13 +31,47 @@ function readName() {
     return userName
 }
 
+// Добавление в заголовок имени польщзователя
 function setNameHeader(name) {
-    $(".section-outer-game__question").text(name + ", угадай мое слово, называя буквы")
+    $(".section-outer-game__question p").text(name + ", угадай мое слово, называя буквы")
 }
 
+// 
 function startGame() {
-    $('.section-outer-game__input').hide();
-    $('.section-outer-game__main').show();
+    $('.section-outer-game__input').fadeOut(2000, function(){
+        $('.section-outer-game-main').fadeIn(2000);
+    })
+   
+    let word = getRandomWord()
+    let letterArray = word.split("")
+    wordInfo.word = letterArray
+    showWord()
 }
+
+// Выводит * или буквы в зависимости от угаданных пользователем букв
+function showWord() {
+    let userWord = ""
+    for (let i = 0; i < wordInfo.word.length; i++) {
+        if (wordInfo.guessIndexes.includes(i)){
+            userWord += wordInfo.word[i]
+        } else {
+            userWord += "*"
+        }
+    }
+    $("#word").text(userWord)
+    console.log("Сгенерированное слово: " + wordInfo.word)
+}
+
+// Выбирает рандомное слово
+function getRandomWord() {
+    return words[Math.floor(Math.random() * words.length)];
+}
+
+
+
+
+
+
+
 
 $("#start-button").on("click", readAndStart)
