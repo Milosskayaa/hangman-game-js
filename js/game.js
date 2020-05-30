3eb881e85390f6b2e9f23cf28523441a3d9590ae
NKRUS <nik1rus2moscow3@gmail.com>
2020-05-30 21:37:38 +0300

    Добавлен алгоритм угадывания букв загаданного слова

--- js/game.js
+++ js/game.js
@@ -31,15 +31,19 @@ function readName() {
     return userName
 }
 
-// Добавление в заголовок имени польщзователя
+// Добавление в заголовок имени пользователя
 function setNameHeader(name) {
     $(".section-outer-game__question p").text(name + ", угадай мое слово, называя буквы")
 }
 
 // 
 function startGame() {
-    $('.section-outer-game__input').fadeOut(2000, function(){
-        $('.section-outer-game-main').fadeIn(2000);
+    $(".section-outer-game__button").fadeOut(1000, function(){
+        $(".section-outer-game__button-guess").fadeIn(1000)
+    })
+    $('.section-outer-game__input').fadeOut(1000, function(){
+        $('.section-outer-game-main').fadeIn(1000)
+        $("#letter-input").focus()
     })
    
     let word = getRandomWord()
@@ -67,11 +71,55 @@ function getRandomWord() {
     return words[Math.floor(Math.random() * words.length)];
 }
 
+// Сделать ход
+function makeGuess(){
+    let userInput = $("#letter-input").val()
+    $("#letter-input").val("")
+    if (userInput.length == 0) {
+        alert("Строка пустая, введите букву или слово")
+        return
+    }
+    if (userInput.length == 1) {
+        let indexes = getAllIndexes(wordInfo.word, userInput)
+        if (indexes.length == 0) {
+            alert("Такой буквы нет в слове")
+        } else {
+            wordInfo.guessIndexes = wordInfo.guessIndexes.concat(indexes)
+        }
+    } else {
 
+    }
+    showWord()
+    $("#letter-input").focus()
+    if(isGameDone()){
+        setTimeout(function() { alert("Урааа! Вы угадали слово!") }, 1);
+    }
+}
 
+// Возврат индексов переданной буквы в загаданном слове
+// Возвращает массив индексов буквы или возвращает пустой массив в случае, если такой буквы нет
+function getAllIndexes(arr, val) {
+    var indexes = [], i = -1;
+    while ((i = arr.indexOf(val, i+1)) != -1){
+        indexes.push(i);
+    }
+    return indexes;
+}
 
+function isGameDone(){
+    for (let i = 0; i < wordInfo.word.length; i++) {
+        //Если такой индекс еще не угадан, вернем false(игра еще идет)
+        if(wordInfo.guessIndexes.indexOf(i) == -1){
+            return false
+        }
+    }
+    return true
+}
 
 
 
 
 $("#start-button").on("click", readAndStart)
+$("#guess-button").on("click", makeGuess)
+
+$("#name-input").focus()
